aca7aa39b13fb3c3acf08a9bfba8e451cb6fecad
来碗麻辣烫 <59546398+xdx888999@users.noreply.github.com>
2026-03-27 00:43:40 +0800

    Add aic formula

---
+++ Formula/aic.rb
@@ -0,0 +1,24 @@
+class Aic < Formula
+  desc "Terminal AI CLI tool manager"
+  homepage "https://github.com/xdx888999/aic"
+  license "MIT"
+  version "0.1.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/xdx888999/aic/releases/download/v0.1.0/aic_darwin_arm64.tar.gz"
+      sha256 "a7370265cbf0d789a784e0dfafd5b52059fd3820de78e793527995276032aae3"
+    else
+      url "https://github.com/xdx888999/aic/releases/download/v0.1.0/aic_darwin_amd64.tar.gz"
+      sha256 "5eb9502c8bdf273e217ab0c661f7b029ed10ee9e2b6c2199d54afea5f8288cbe"
+    end
+  end
+
+  def install
+    bin.install "aic"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/aic --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-tap
+
+Homebrew tap for `aic`.
+
+## Install
+
+```bash
+brew tap xdx888999/homebrew-tap
+brew install aic
+```
